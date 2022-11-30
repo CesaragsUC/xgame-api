f301db8cc5326944fd5ed8b4629654aed7440751
cesar augusto gadelha <cesar_ags@outlook.com>
2022-11-29 21:55:54 -0300

    correção msg de erros

--- Simple API/src/mvc/Models/ProdutoModel.cs
+++ Simple API/src/mvc/Models/ProdutoModel.cs
@@ -7,7 +7,7 @@ namespace mvc.Models
         public Guid Id { get; set; }
         public string Nome { get; set; }
 
-        public string Imagem { get; set; }
+        public string? Imagem { get; set; }
         public IFormFile ImagemUpload { get; set; }
         public decimal Valor { get; set; }
         public int Quantidade { get; set; }
